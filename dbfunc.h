f4c874d35b02b716525ea4de50899c033a14de6b
VarchukVladimir <fildes@gmail.com>
2014-03-16 14:09:22 +0200

    first commit

---
+++ dbfunc.h
@@ -0,0 +1,37 @@
+/*
+ * dbfunc.h
+ *
+ *  Created on: Feb 12, 2014
+ *      Author: Volodymyr Varchuk
+ */
+
+#ifndef DBFUNC_H_
+#define DBFUNC_H_
+
+#include <sqlite3.h>
+#include "strfunc.h"
+
+
+#define LOAD_LOCAL "/home/volodymyr/workspace/SQLite_search/load_save/load"
+#define SAVE_LOCAL "/home/volodymyr/workspace/SQLite_search/load_save/save"
+#define LOAD_ZEROCOLUD "/dev/input"
+#define SAVE_ZEROCOLUD "/dev/output"
+#define DB_DIR_ZEROCLOUD "/db"
+#define DB_DIR_LOCAL "/home/volodymyr/workspace/SQLite_search/load_save/db"
+#define DB_FILE "metatags.db"
+#define DB_TABLE_NAME "metatags_tab"
+#define CONTETN_LENGTH_NAME "content_length"
+
+
+
+int create_Table (sqlite3 *, List_t *);
+void databaseError(sqlite3*);
+int db_open (const char *, sqlite3 *);
+int get_tables_names (sqlite3 *, List_t *);
+int delete_table (sqlite3 *, const char *);
+int get_table_struct (sqlite3 *, KeyValueList_t *, const char *);
+int insertValues (sqlite3 *, IntKeyValueList_t *, KeyValueList_t *, KeyValueList_t *, const char *);
+int Query_SQL (sqlite3 *);
+int checkTable (sqlite3 *, char *);
+
+#endif /* DBFUNC_H_ */
