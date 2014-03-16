e1b29aea9377fa8a65b3ab3b7ea73d330d5ea6c8
VarchukVladimir <fildes@gmail.com>
2014-03-16 15:43:03 +0200

    first commit

---
+++ opts.h
@@ -0,0 +1,26 @@
+/*
+ * opts.h
+ *
+ *  Created on: Feb 5, 2014
+ *      Author: volodymyr varchuk
+ */
+
+#ifndef OPTS_H_
+#define OPTS_H_
+
+#include "strfunc.h"
+
+typedef enum {add, delete, find, struct_create, none} Operation_t;
+
+typedef struct {
+	Operation_t tMode;
+	char *pObject_path;
+	int iList;
+	char **pKey;
+	char **pVal;
+	int iCount;
+} Options_t;
+
+int getopts (int, char **, Options_t*, KeyValueList_t *, List_t *);
+
+#endif /* OPTS_H_ */
